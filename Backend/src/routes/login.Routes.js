a35cc52589d77b5f3900de75090bef6206d1dce3
TheDataMaven1985 <favourkolawole1985@gmail.com>
2026-09-03 15:18:11 +0100

    Added refreshToken routes

--- Backend/src/routes/login.Routes.js
+++ Backend/src/routes/login.Routes.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const validate = require('../middleware/validate.Middleware');
 const { loginSchema } = require('../validators/auth.Validator');
 const { login } = require('../controllers/login.Controller');
+const { refreshAccessTokenController } = require('../controllers/refreshToken.Controller');
 
 router.post('/login', validate(loginSchema), login);
+router.post('/refresh', refreshAccessTokenController);
 
 module.exports = router;
