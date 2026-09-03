ae6ab5d5acff9bf79ac6419a36101eb532aa9e90
TheDataMaven1985 <favourkolawole1985@gmail.com>
2026-09-03 15:19:44 +0100

    cookie-parser added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const cookieParser = require('cookie-parser')
 const routes = require('./routes/index');
 const notFound = require('./middleware/notFound.Middleware');
 const errorHandler = require('./middleware/errorHandler.Middleware');
@@ -7,6 +8,7 @@ const errorHandler = require('./middleware/errorHandler.Middleware');
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use('/api/v1', routes);
 
