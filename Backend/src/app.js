1881b5d5081178e1121bcec9ccdfbf20fd9bdda1
TheDataMaven1985 <favourkolawole1985@gmail.com>
2026-09-03 07:47:00 +0100

    App.js fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require('express');
 
-app.use(express.json())
+const routes = require('./routes/index');
+const notFound = require('./middleware/notFound.Middleware');
+const errorHandler = require('./middleware/errorHandler.Middleware');
 
-app.get("/health", (req, res) => {
-    res.status(200).json({ message: "OK" })
-})
+const app = express();
 
-module.exports = app
+app.use(express.json());
+
+app.use('/api/v1', routes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
