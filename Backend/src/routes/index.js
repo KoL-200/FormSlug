216cd8f6f71175ba73682ea2264bce1aeef7622c
TheDataMaven1985 <favourkolawole1985@gmail.com>
2026-09-04 07:20:03 +0100

    Logout routes added

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const healthRoutes = require('./health.Routes');
 const registerRoutes = require('./register.Routes');
 const loginRoutes = require('./login.Routes');
+const logoutRoutes = require('./logout.Routes');
 
 const router = express.Router();
 
 router.use('/', healthRoutes);
 router.use('/auth', registerRoutes);
 router.use('/auth', loginRoutes);
+router.use('/auth', logoutRoutes);
 
 module.exports = router;
