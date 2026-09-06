659777bfcf649cab7b1ac61f0170e4ba0f34c2b9
TheDataMaven1985 <favourkolawole1985@gmail.com>
2026-09-06 05:09:07 +0100

    projectRoutes added

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const healthRoutes = require('./authenticationRoutes/health.Routes');
+
+// Authentication Routes
 const registerRoutes = require('./authenticationRoutes/register.Routes');
 const loginRoutes = require('./authenticationRoutes/login.Routes');
 const logoutRoutes = require('./authenticationRoutes/logout.Routes');
 const getMeRoutes = require('./authenticationRoutes/getMe.Routes');
 const updateMeRoutes = require('./authenticationRoutes/updateMe.Routes');
 
+// Project Routes
+const createprojectRoutes = require('./projectRoutes/createProject.Routes')
+
+
 const router = express.Router();
 
 router.use('/', healthRoutes);
+
+// Authentication
 router.use('/auth', registerRoutes);
 router.use('/auth', loginRoutes);
 router.use('/auth', logoutRoutes);
 router.use('/', getMeRoutes);
 router.use('/', updateMeRoutes);
+
+// Project
+router.use('/', createprojectRoutes)
 
 module.exports = router;
