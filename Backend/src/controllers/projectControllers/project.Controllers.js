a03b704ede65221d3d8f3ca3e83829853cb4beb8
TheDataMaven1985 <favourkolawole1985@gmail.com>
2026-09-07 05:50:51 +0100

    createForm and getForms Contoller added

--- Backend/src/controllers/projectControllers/project.Controllers.js
+++ Backend/src/controllers/projectControllers/project.Controllers.js
@@ -1,4 +1,5 @@
 const { createProject } = require('../../services/projectServices/createProject.Services');
+const { getProjects } = require('../../services/projectServices/getProjects.Services');
 
 const createProjectController = async (req, res) => {
     const userId = req.user.id;
@@ -8,4 +9,10 @@ const createProjectController = async (req, res) => {
     res.status(201).json({ success: true, data: createdProject });
 };
 
-module.exports = { createProjectController };
+const getProjectsController = async (req, res) => {
+    const projects = await getProjects({ userId: req.user.id });
+
+    res.status(200).json({ success: true, data: projects });
+};
+
+module.exports = { createProjectController, getProjectsController };
