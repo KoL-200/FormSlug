3c118b69da1d7b7dc905a5e9f21a03f6c98aab2a
TheDataMaven1985 <favourkolawole1985@gmail.com>
2026-09-09 04:01:36 +0100

    New routes added:  update and delete project and form

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -11,7 +11,11 @@ const updateMeRoutes = require('./authenticationRoutes/updateMe.Routes');
 // Project Routes
 const createProjectRoutes = require('./projectRoutes/createProject.Routes')
 const getProjectsRoutes = require('./projectRoutes/getProjects.Routes')
+const updateProjectRoutes = require('./projectRoutes/updateProject.Routes')
+const deleteProjectRoutes = require('./projectRoutes/deleteProject.Routes')
 const createFormRoutes = require('./projectRoutes/createForm.Routes')
+const updateFormRoutes = require('./projectRoutes/updateForm.Routes')
+const deleteFormRoutes = require('./projectRoutes/deleteForm.Routes')
 
 
 const router = express.Router();
@@ -28,6 +32,10 @@ router.use('/', updateMeRoutes);
 // Project
 router.use('/', createProjectRoutes)
 router.use('/', getProjectsRoutes)
+router.use('/', updateProjectRoutes)
+router.use('/', deleteProjectRoutes)
 router.use('/', createFormRoutes);
+router.use('/', updateFormRoutes);
+router.use('/', deleteFormRoutes);
 
 module.exports = router;
